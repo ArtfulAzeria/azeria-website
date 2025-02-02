54d99f5ff8b60a79cfa1ab651725471ba39d2281
Azeria <azeriaketturenard@gmail.com>
2025-02-02 03:20:58 +0100

    fix background with overlay + icons from devicon

--- conf/locale/en-GB/en-GB_locale.js
+++ conf/locale/en-GB/en-GB_locale.js
@@ -4,8 +4,9 @@ enGB = {
     "mylangs_1": "Spanish - Native",
     "mylangs_2":"English - Advanced",
     "open-source-personal": "My open source projects",
-    "osp-1": "BlueSky bot that posts Spain meteorological alerts given by AEMET.",
-    "osp-2": "BlueSky bot that posts daily a countdown to new year.",
+    "osp-1": "The web page you are currently seeing.",
+    "osp-2": "BlueSky bot that posts Spain meteorological alerts given by AEMET.",
+    "osp-3": "BlueSky bot that posts daily a countdown to new year.",
     "open-source-contrib": "My open source contributions",
     "osc-1": "Translation and proofreading.",
     "osc-2": "Visualization bug fixing."
--- conf/locale/es-ES/es-ES_locale.js
+++ conf/locale/es-ES/es-ES_locale.js
@@ -4,8 +4,9 @@ esES = {
     "mylangs_1": "Español - Nativo",
     "mylangs_2": "Inglés - Avanzado",
     "open-source-personal": "Mis proyectos de código abierto",
-    "osp-1": "Bot para BlueSky que publica las alertas meteorológicas de España dadas por la AEMET.",
-    "osp-2": "Bot para BlueSky que publica diariamente una cuenta atrás para año nuevo.",
+    "osp-1": "La página web que estás viendo actualmente",
+    "osp-2": "Bot para BlueSky que publica las alertas meteorológicas de España dadas por la AEMET.",
+    "osp-3": "Bot para BlueSky que publica diariamente una cuenta atrás para año nuevo.",
     "open-source-contrib": "Mis contribuciones de código abierto",
     "osc-1": "Traducción y corrección de textos.",
     "osc-2": "Corrección de errores visuales."
